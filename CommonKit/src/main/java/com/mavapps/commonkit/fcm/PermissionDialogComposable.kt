5485533cc68f6e18489a5dcba11c84fe5ea31534
amarjit <amarjitkr93@gmail.com>
2025-08-25 17:52:31 +0530

    fixed strings issue

--- CommonKit/src/main/java/com/mavapps/commonkit/fcm/PermissionDialogComposable.kt
+++ CommonKit/src/main/java/com/mavapps/commonkit/fcm/PermissionDialogComposable.kt
@@ -10,9 +10,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.mavapps.commonkit.R
 import com.mavapps.commonkit.utils.alertDialog
 import com.mavapps.commonkit.utils.textButton
-import com.stickmoji.R.string as AppText
+
 
 @Composable
 fun PermissionDialog(onRequestPermission: () -> Unit) {
@@ -21,8 +22,8 @@ fun PermissionDialog(onRequestPermission: () -> Unit) {
     if (showWarningDialog) {
         AlertDialog(
             modifier = Modifier.alertDialog(),
-            title = { Text(stringResource(id = AppText.notification_permission_title)) },
-            text = { Text(stringResource(id = AppText.notification_permission_description)) },
+            title = { Text(stringResource(id = R.string.notification_permission_title)) },
+            text = { Text(stringResource(id = R.string.notification_permission_description)) },
             confirmButton = {
                 TextButton(
                     onClick = {
@@ -31,7 +32,7 @@ fun PermissionDialog(onRequestPermission: () -> Unit) {
                     },
                     modifier = Modifier.textButton(),
 
-                ) { Text(text = stringResource(AppText.request_notification_permission)) }
+                ) { Text(text = stringResource(R.string.request_notification_permission)) }
             },
             onDismissRequest = { }
         )
@@ -45,13 +46,13 @@ fun RationaleDialog() {
     if (showWarningDialog) {
         AlertDialog(
             modifier = Modifier.alertDialog(),
-            title = { Text(stringResource(id = AppText.notification_permission_title)) },
-            text = { Text(stringResource(id = AppText.notification_permission_settings)) },
+            title = { Text(stringResource(id = R.string.notification_permission_title)) },
+            text = { Text(stringResource(id = R.string.notification_permission_settings)) },
             confirmButton = {
                 TextButton(
                     onClick = { showWarningDialog = false },
                     modifier = Modifier.textButton(),
-                ) { Text(text = stringResource(AppText.ok)) }
+                ) { Text(text = stringResource(R.string.ok)) }
             },
             onDismissRequest = { showWarningDialog = false }
         )
